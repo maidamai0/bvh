00a1a9dc0fc57c5a7e195065be73020fbaaf9216
tonghao.yuan <yuantonghao@gmail.com>
2024-05-29 22:00:33 +0800

    feat: parallel

--- sah.cpp
+++ sah.cpp
@@ -1,10 +1,14 @@
 #include "sah.hpp"
 
+#include <algorithm>
 #include <array>
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
+#include <execution>
 #include <fstream>
+#include <numeric>
+#include <vector>
 
 #include "viewer.h"
 
@@ -39,23 +43,27 @@ int main(int argc, char** argv) {
   sah_bvh bvh(triangles);
   run("sah bvh", 640, 640, [&](Surface& canvas) {
     timer timer;
+
     canvas.Clear(0);
     float3 cam_pos(-1.5f, -0.2f, -2.5f);
     float3 p0(-2.5f, 0.8f, -0.5f);
     float3 p1(-0.5f, 0.8f, -0.5f);
     float3 p2(-2.5f, -1.2f, -0.5f);
-    for (int y = 0; y < canvas.height; ++y) {
-      for (int x = 0; x < canvas.width; ++x) {
-        float u = x / float(canvas.width);
-        float v = y / float(canvas.height);
-        float3 pixel_pos = p0 + (p1 - p0) * u + (p2 - p0) * v;
-        ray r = ray{cam_pos, normalize(pixel_pos - cam_pos)};
-        // bvh.intersect(r);
-        for (auto& t : triangles) intersect_tri(t, r);
-        uint32_t c = 500 - (int)(r.t * 60);
-        if (r.t < 1e30f) canvas.Plot(x, y, c * 0x10101);
-      }
-    }
+
+    int len = canvas.height * canvas.width;
+    std::vector<int> indices(len);
+    std::iota(indices.begin(), indices.end(), 0);
+    std::for_each_n(std::execution::par, indices.begin(), len, [&](int& i) {
+      int x = i % canvas.width;
+      int y = i / canvas.width;
+      float u = x / float(canvas.width);
+      float v = y / float(canvas.height);
+      float3 pixel_pos = p0 + (p1 - p0) * u + (p2 - p0) * v;
+      ray r = ray{cam_pos, normalize(pixel_pos - cam_pos)};
+      bvh.intersect(r);
+      uint32_t c = 500 - (int)(r.t * 20);
+      if (r.t < 1e30f) canvas.Plot(x, y, c * 0x10101);
+    });
 
     std::println("tracing time: {}ms ({}K rays/s)", timer.elapsed(),
                  float(canvas.width * canvas.height) / timer.elapsed());
